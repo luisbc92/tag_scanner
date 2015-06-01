790b2932492769a7fe10726f00926f6721e27866
luisbc92 <luiscarlos.banuelos@gmail.com>
2015-06-01 12:36:53 -0700

    Rewrite

--- tag_scanner.py
+++ tag_scanner.py
@@ -1,10 +1,24 @@
-# coding=utf-8
-__author__ = "Luis Bañuelos"
-__license__ = "MIT"
-__version__ = "2015-05-28"
-__email__ = "luiscarlos.banuelos@gmail.com"
+#!/usr/bin/env python
+# coding = utf-8
+__author__	= "Luis Carlos Bañuelos Chacon"
+__license__ = "TBD"
+__version__	= "06/01/2015"
+__email__	= "luiscarlos.banuelos@gmail.com"
 
-import bglib, serial, time, datetime
+import bglib, serial, re
+
+tag_discovered = []		# MAC addresses of identified tags
+tag_data = []				# data collected from tags
+
+# function to get MAC address of a network interface (Linux only)
+def get_mac(interface):
+	# get mac address as string
+	try:
+		mac = open("/sys/class/net/%s/address" %interface).readline()
+	except:
+		mac = "00:00:00:00:00:00"
+	# separate tokens and return
+	return mac.rstrip().split(':')
 
 # handler to notify of an API parser timeout condition
 def ble_parser_timeout(sender, args):
@@ -12,14 +26,44 @@ def ble_parser_timeout(sender, args):
 
 # handler to catch scan responses
 def ble_evt_gap_scan_response(sender, args):
-	if (args["packet_type"] == 4):
-		disp = []
-		disp.append("%s" % ''.join(['%02X' % b for b in args["sender"][::-1]]))
-		disp.append("%d" % args["rssi"])
-		disp.append("%s" % ''.join(['%02X' % b for b in args["data"][::-1]]))
-		print ' '.join(disp)
+	global tag_data
+	global tag_discovered
+
+	# get advertisement packet and look for tags
+	if (args["packet_type"] == 0):							# 0 - connecteable advertisement packet
+		ble_name = ''.join("%c" %c for c in args["data"])	# convert all data into a string to extract name (lazy)
+		ble_name = re.sub(r"\W+", "", ble_name)				# remove everything but characters (even more lazy)
+		if (ble_name.find("TAG") == 0):						# if name begins with "TAG"
+			if not (args["sender"] in tag_discovered):		# and it has not already been discovered
+				tag_discovered.append(args["sender"])		# add MAC address into list
+				print "Discovered TAG: " + ble_name
+
+	# get scan response packet and extract data
+	# packet structure (30 bytes)
+	# byte 0  	= packet length	(ignored)
+	# byte 1  	= packet type 	(ignored)
+	# byte 2-28	= data payload
+	# byte 29	= packet count 	(used to identify retransmitted packets)
+	if (args["packet_type"] == 4 and args["sender"] in tag_discovered and len(args["data"]) == 30):	# it has to be 30 bytes
+		if not any(args["data"][29] == p["count"] and args["sender"] == p["id"] for p in tag_data):	# discard if packet is a retransmission
+			rx["mac"] 	= args["sender"]					# MAC address
+			rx["count"] = args["data"][29]					# byte 29 is packet count
+			rx["rssi"]	= args["rssi"]						# RSSI
+			rx["data"]	= args["data"][2:29]				# data payload
+			tag_data.append(rx)								# append received packet
+
+# function to prepare packet for transmission
+def create_packet():
+	global tag_data
+
+	mac = get_mac("eth0")								# obtain mac address
+	packet = {"ant_mac": mac, "tag_data": tag_data}		# join antenna mac address and collected data
+	return packet
+
 
 def main():
+	global tag_data
+
 	# port configuration
 	port_name = "com11"
 	baud_rate = 115200
@@ -59,6 +103,10 @@ def main():
 	while (1):
 		# check for all incoming data (no timeout, non-blocking)
 		ble.check_activity(ser)
+
+		if (len(tag_data) > 128)
+			print create_packet()
+			tag_data = []
 
 		time.sleep(0.01)
 
